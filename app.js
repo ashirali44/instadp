c76ad43651899245fe9d20cff656a64984f04b9a
NEXTBRIDGE\ashir.muhammad <muhammadashirali4@gmail.com>
2022-12-03 01:45:16 +0500

    JHGJ

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const instaRotues = require('./src/instagram');
 
 
-
 const app = express()
 const port = 3000
 
-
-
-
-
-
-
-app.use('/api/insta', instaRotues);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/instagram', instaRotues);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
