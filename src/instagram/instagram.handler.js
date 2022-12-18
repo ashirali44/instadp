5f94391bc04f6a421a83bc1694f47903043e4925
Ashir Ali <muhammadashirali@icloud.com>
2022-12-18 17:38:30 +0500

    asdfa

--- src/instagram/instagram.handler.js
+++ src/instagram/instagram.handler.js
@@ -11,90 +11,98 @@ const ig = new IgApiClient();
 
 exports.adminLogin = async function (req, res) {
 
-   var status = fileHandler.deleteFile();
-   if(!status){
-    var result = {
-        status: false,
-        type: '-'
-    }
-    try {
-        if (req.body.loginPassword == 'ALIali123!') {
-            ig.state.generateDevice(account.USERNAME);
-           
+    var status = fileHandler.deleteFile();
+    if (!status) {
+        var result = {
+            status: false,
+            type: '-'
+        }
+        try {
+            if (req.body.loginPassword == 'ALIali123!') {
+                ig.state.generateDevice(account.USERNAME);
+
                 await ig.account.login(account.USERNAME, account.PASSWORD);
                 console.log('Logging In Via ID-PASS');
                 result.status = true;
                 result.type = 'Via UserName Password';
-            
-            res.json(result);
-        } else {
+
+                res.json(result);
+            } else {
+                result.status = false;
+                result.type = 'Wrong Password';
+                res.json(result);
+
+
+            }
+
+        } catch (e) {
             result.status = false;
-            result.type = 'Wrong Password';
-            res.json(result);
-
-
+            result.type = e;
+            console.log(e);
         }
-
-    } catch (e) {
-        result.status = false;
-        result.type = e;
-        console.log(e);
+    } else {
+        var result = {
+            status: false,
+            type: 'Unable to Delete File'
+        }
+        res.json(result);
     }
-   }else{
-    var result = {
-        status: false,
-        type: 'Unable to Delete File'
-    }
-    res.json(result); 
-   }
 
 }
 
 exports.fetchUserData = async function (req, res) {
     var searchedUsers;
     let errorOccurred = false;
-    let requestedUsername = '';
-    const rx = /^(?:@|(?:https?:\/\/)?(?:www\.)?instagr(?:\.am|am\.com)\/)?(\w+)\/?$/
-    let match = rx.exec(req.query.username) 
-    if (match) {
-        requestedUsername = match[1];
-    }
-    try {
+    let requestedUsername = req.query.username;
+    if (requestedUsername.includes("instagram.com")) {
+        const rx = /(?:(?:http|https):\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am)\/([A-Za-z0-9-_\.]+)/im;
+        let match = rx.exec(req.query.username)
+        console.log(match);
+        if (match) {
+            requestedUsername = match[1];
+            console.log(requestedUsername);
+        }
+    } 
+    // try {
 
-        ig.state.generateDevice(account.USERNAME);
-        if (await fileHandler.existFile()) {
-            console.log('VIA Cookies');
-            try {
-                await ig.state.deserialize(await fileHandler.loadFile());
-                searchedUsers = (await ig.user.info(
-                    await ig.user.getIdByUsername(requestedUsername))
-                );
-            } catch (e) {
-                console.log(e);
-                searchedUsers = {
-                    error : errorOccurred,
-                    message : "Unable to Find the requested Profile",
-                    exception : e.toString()
-                }
-            }
-        } 
+    //     ig.state.generateDevice(account.USERNAME);
+    //     if (await fileHandler.existFile()) {
+    //         console.log('VIA Cookies');
+    //         try {
+    //             await ig.state.deserialize(await fileHandler.loadFile());
+    //             searchedUsers = (await ig.user.info(
+    //                 await ig.user.getIdByUsername(requestedUsername))
+    //             );
 
-        res.send(searchedUsers);
+    //         } catch (e) {
 
-    } catch (e) {
-        console.log(e.message)
-        res.status(500).send({ error: 'Problem fetching books.' });
-    }
+    //                 console.log(e);
+    //                 searchedUsers = {
+    //                     error: errorOccurred,
+    //                     message: "Unable to Find the requested Profile",
+    //                     exception: e.toString()
+    //                 }
+
+    //         }
+    //     }
+
+    //     res.send(searchedUsers);
+
+    // } catch (e) {
+    //     console.log(e.message)
+    //     res.status(500).send({ error: 'Problem fetching books.' });
+    // }
+    res.send(requestedUsername);
 }
 
 exports.fetchMediaData = async function (req, res) {
     var mediatempid;
     const b = Buffer.from(req.query.id);
 
-    if(b.includes('reel')){
-         mediatempid = betweenMarkers("/reel","/",req.query.id);
-    }else{
-        mediatempid = betweenMarkers("/p","/",req.query.id);
+    if (b.includes('reel')) {
+        mediatempid = betweenMarkers("/reel", "/", req.query.id);
+    } else {
+        mediatempid = betweenMarkers("/p", "/", req.query.id);
     }
 
     var mediadata;
@@ -134,10 +142,10 @@ ig.request.end$.subscribe(async () => {
 });
 
 
-function betweenMarkers( begin, end, originalText) {
+function betweenMarkers(begin, end, originalText) {
     var buf = Buffer.from(originalText);
-    var firstChar = buf.indexOf(begin) + begin.length +1;
+    var firstChar = buf.indexOf(begin) + begin.length + 1;
     var lastChar = buf.lastIndexOf(end);
     var newText = originalText.substring(firstChar, lastChar);
     return newText;
-  }
+}
