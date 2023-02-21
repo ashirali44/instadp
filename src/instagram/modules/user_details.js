5ef579e26027b5f47809281919cdd48030027ffc
NEXTBRIDGE\ashir.muhammad <muhammadashirali4@gmail.com>
2023-02-21 20:26:01 +0500

    asdfalskf

--- src/instagram/modules/user_details.js
+++ src/instagram/modules/user_details.js
@@ -2,9 +2,12 @@
 // const PASSWORD = 'ALIali899)';
 
 
-const USERNAME = '_f8do';
-const PASSWORD = 'ALIali899';
+// const USERNAME = '_f8do';
+// const PASSWORD = 'ALIali899';
 
+
+const USERNAME = 'instadps.live';
+const PASSWORD = 'ALIali899';
 
 module.exports = {
     USERNAME, PASSWORD
