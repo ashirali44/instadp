735cf78d1370758fde51d21a68317fa8e4b3cbbc
NEXTBRIDGE\ashir.muhammad <muhammadashirali4@gmail.com>
2023-03-22 22:50:32 +0500

    asdf

--- src/instagram/modules/user_details.js
+++ src/instagram/modules/user_details.js
@@ -1,13 +1,13 @@
-const USERNAME = 'flutterfind';
-const PASSWORD = 'ALIali899)';
+// const USERNAME = 'flutterfind';
+// const PASSWORD = 'ALIali899)';
 
 
 // const USERNAME = '_f8do';
 // const PASSWORD = 'ALIali899';
 
 
-// const USERNAME = 'instadps.live';
-// const PASSWORD = 'ALIali899';
+const USERNAME = 'instadps.live';
+const PASSWORD = 'ALIali899';
 
 module.exports = {
     USERNAME, PASSWORD
